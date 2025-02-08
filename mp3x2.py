a531163444a578baa9dea82f47d9080747b2eeee
Space <143919787+Rhcs2023@users.noreply.github.com>
2025-02-07 23:01:28 -0600

    Update mp3x2.py

--- mp3x2.py
+++ mp3x2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from gtts import gTTS
 import tempfile
+from pydub import AudioSegment
 
 # Diccionario con palabras a traducir
 diccionario = {
@@ -28,6 +29,16 @@ def reproducir_audio(texto, lang):
         tts.save(tmp.name)
         return tmp.name  # Retorna la ruta del archivo temporal
 
+def cambiar_velocidad_audio(audio_path, velocidad):
+    # Cargar el archivo de audio
+    audio = AudioSegment.from_file(audio_path)
+    # Cambiar la velocidad
+    nuevo_audio = audio.speedup(playback_speed=velocidad)
+    # Guardar el nuevo archivo
+    nuevo_path = audio_path.replace('.mp3', f'_velocidad_{velocidad}.mp3')
+    nuevo_audio.export(nuevo_path, format='mp3')
+    return nuevo_path
+
 st.title("Texto a Voz")
 
 # Estado de la sesión para la traducción
@@ -37,20 +48,28 @@ if 'oracion_traducida' not in st.session_state:
 # Opción para introducir texto
 oracion_usuario = st.text_area("Introduce una oración :", height=200)
 
+# Opción para seleccionar la velocidad
+velocidad = st.slider("Selecciona la velocidad de reproducción:", 1.0, 2.0, 1.25)
+
 # Botón para ejecutar la traducción
 if st.button("ir"):
     if oracion_usuario:
         oracion_traducida = traducir_oracion(oracion_usuario)
         st.session_state.oracion_traducida = oracion_traducida
         audio_file_path = reproducir_audio(oracion_traducida, 'es')  # Usando español por defecto
-        st.audio(audio_file_path, format='audio/mp3')
+        
+        # Cambiar la velocidad del audio
+        audio_file_path_modificado = cambiar_velocidad_audio(audio_file_path, velocidad)
+        
+        # Reproducir el audio modificado
+        st.audio(audio_file_path_modificado, format='audio/mp3')
 
-        # Botón para descargar el archivo de audio
-        with open(audio_file_path, 'rb') as audio_file:
+        # Botón para descargar el archivo de audio modificado
+        with open(audio_file_path_modificado, 'rb') as audio_file:
             st.download_button(
                 label="Descargar audio en MP3",
                 data=audio_file,
-                file_name="audio.mp3",
+                file_name=f"audio_velocidad_{velocidad}.mp3",
                 mime="audio/mp3"
             )
     else:
